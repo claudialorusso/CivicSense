509df2e15526a312a423b1c8d336585d2a5140bd
claudialorusso <lorusso.claudia@outlook.it>
2022-05-24 13:32:41 +0200

    Possible Path Manipulation Fix

--- Admin/InserisciDati.php
+++ Admin/InserisciDati.php
@@ -25,22 +25,39 @@
 		$lat = floatval($lat);
 		$lng = $_POST['longitudine'];
 		$lng = floatval($lng);
-		
 
 		try{
-			move_uploaded_file($_FILES['image']['tmp_name'],$file_path);
-			$sql = "INSERT INTO `segnalazioni`(`datainv`, `orainv`, `via`, `descrizione`, `foto`, `email`,`tipo`,`latitudine`,`longitudine`) 
-			VALUES (CURRENT_DATE,CURRENT_TIME,'".$via."','".$descrizione."','{$img_name}','".$email."','".$tipo."',".$lat.",".$lng.")";
-			$result = mysqli_query($conn,$sql);
-			if($result){
-				echo "Inserimento dei dati completato";
-			}
-			else{
-				echo "Errore nell'inserimento dei dati";
-			}
+            $file_path = realpath($file_path);
+            if(!$file_path) {
+                $err_message = "Percorso file non corretto"; //@fixme CLAUDIA
+            } else {
+                # I list all of the jpg files contained in "jpeg/"
+                $file_list = glob($upload_path . '*.jpg');
+                #array_push(@$file_list, glob($upload_path . '*.png')); //@fixme CLAUDIA I don't know if it's needed
+                #checks whether the image is real or not
+                $real = false;
+                foreach($file_list as $filename){
+                    if(realpath($filename)==$file_path){
+                        $real=true; #YAY! I founded the image! The path is valid!
+                        break;
+                    }
+                }
+                if($real){
+                    $base_filename = basename($file_path);
+                    move_uploaded_file($_FILES['image']['tmp_name'], "'$upload_path' . '$base_filename'"); //@TODO CLAUDIA Check
+                    $sql = "INSERT INTO 'segnalazioni'('datainv', 'orainv', 'via', 'descrizione', 'foto', 'email','tipo','latitudine','longitudine') VALUES (CURRENT_DATE,CURRENT_TIME,'".$via."','".$descrizione."','{$img_name}','".$email."','".$tipo."',".$lat.",".$lng.")";
+                    $result = mysqli_query($conn,$sql);
+                    if($result){
+                        echo "Inserimento dei dati completato";
+                    }
+                    else{
+                        echo "Errore nell'inserimento dei dati";
+                    }
+                }
 
+            }
 		}catch(Exception $e){
-            $e->getMessage();
+            $e->getMessage(); // @TODO CLAUDIA error message needs to be managed
         }
         $conn->close();
 	}
